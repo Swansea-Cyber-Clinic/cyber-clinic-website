e0517d6f3a51666a235d303b5ad8295d07f20e50
Genevieve Clifford <genevieve@becquerel.me>
2022-01-25 18:04:11 +0000

    changed webpack config to point to js folder in public

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const publicPath =
-  process.env.NODE_ENV === "production" ? "/js/" : "/";
+  process.env.NODE_ENV === "production" ? "/" : "/";
 
 module.exports = {
   publicPath,
