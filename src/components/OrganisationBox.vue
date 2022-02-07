efcf9b5621b9982786fff9ad6cccbaec41286011
Genevieve Clifford <genevieve@becquerel.me>
2022-02-07 16:21:14 +0000

    changed pictoral diagrams to text, also added helpline emails

--- src/components/OrganisationBox.vue
+++ src/components/OrganisationBox.vue
@@ -6,17 +6,23 @@
     <p class="is-small has-text-grey">
       {{ organisation.org_description }}
     </p>
-    <a v-if="organisation.org_web" v-bind:href="organisation.org_web" class="is-small">
-      <i class="fas fa-home"></i>
-    </a>
-    <a v-if="organisation.org_email_office" v-bind:href="`mailto:${organisation.org_email_office}`" class="is-small">
-      <i class="fas fa-at"></i>
-    </a>
+    <p v-if="organisation.org_web" v-bind:href="organisation.org_web" class="is-small">
+      <a v-bind:href="organisation.org_web" class="is-small">
+      Website</a>
+    </p>
+    <p v-if="organisation.org_email_office">
+      <a v-bind:href="`mailto:${organisation.org_email_office}`" class="is-small">
+      Email (enquiries)</a>
+    </p>
+    <p v-if="organisation.org_email_help">
+      <a v-bind:href="`mailto:${organisation.org_email_help}`" class="is-small">
+      Email (helpline)</a>
+    </p>
     <p class="is-small has-text-grey" v-if="organisation.org_tel_office">
-      <i class="fas fa-question-circle"></i> {{organisation.org_tel_office }}
+      {{organisation.org_tel_office }} (enquiries)
     </p>
     <p class="is-small has-text-grey" v-if="organisation.org_tel_help">
-      <i class="fas fa-exclamation-circle"></i> {{organisation.org_tel_help }}
+      {{organisation.org_tel_help }} (helpline)
     </p>
   </div>
 </template>
