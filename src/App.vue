3ae93b7e7e861cfa3ae7d010ccea3abff95d5295
Genevieve Clifford <genevieve@becquerel.me>
2022-03-22 14:21:24 +0000

    added locale switcher

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@
         <router-link to="/about" class="navbar-item">{{ $t('pages.about') }}</router-link>
         <router-link to="/team" class="navbar-item">{{ $t('pages.team') }}</router-link>
         <router-link to="/privacy" class="navbar-item">{{ $t('pages.privacy_policy') }}</router-link>
+        <div class="navbar-item">{{ $t('language') }} <LocaleChanger/></div>
       </div>
     </div>
   </nav>
@@ -32,19 +33,22 @@
       <p v-html="$t('branding.blurb_3')"></p>
       <a rel='license' href='http://creativecommons.org/licenses/by-sa/4.0/'><img alt='Creative Commons Licence' style='border-width:0' src='https://i.creativecommons.org/l/by-sa/4.0/80x15.png' /></a>
     </small>
-    
   </footer>
 
 </div>
 </template>
 
 <script>
+import LocaleChanger from './components/LocaleChanger.vue'
 
 export default {
   data () {
     return {
       showMobileMenu: false
     }
+  },
+  components: {
+    LocaleChanger
   }
 }
 
---
+++ src/components/LocaleChanger.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang.code">
+        {{ lang.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LocaleChanger',
+  data() {
+    return {
+      langs: [
+        {
+          'name': 'English',
+          'code': 'en'
+        },
+        {
+          'name': 'Español',
+          'code': 'es'
+        },
+        {
+          'name': 'Cymraeg',
+          'code': 'cy'
+        }
+      ]
+    }
+  }
+}
+</script>
